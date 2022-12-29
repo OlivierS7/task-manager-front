e53004daacb6041a412213e909e6b61d2fbfc7b1
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-12-29 23:35:38 +0100

    Refactoring Services names

---
+++ src/app/services/DynamicDialogService/dynamic-dialog.config.ts
@@ -0,0 +1,16 @@
+import { OverlayConfig } from '@angular/cdk/overlay';
+
+export class DynamicDialogConfig<TData = any> {
+  data?: TData;
+  header = '';
+  closable = true;
+  containerAnimationTiming = 0.3;
+  contentAnimationTiming = 0.2;
+  animationChildDelay?: string | number = 0;
+  overlayConfig?: OverlayConfig;
+  title?: string;
+
+  constructor(private componentTitle?: string) {
+    this.title = componentTitle;
+  }
+}
---
+++ src/app/services/TaskService/task.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Task } from 'src/app/models/task.model';
+import { WebRequestService } from '../WebService/web-request.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private getLists$ = new Subject()
+  private getTasks$ = new Subject()
+
+  constructor(private webRequestService: WebRequestService) {}
+
+  async createList(title: string) {
+    let result = await this.webRequestService.post('lists', {title}).toPromise()
+    this.webRequestService.get('lists').subscribe((lists) => {
+      this.getLists$.next(lists)
+    })
+    return result
+  }
+
+  getLists() {
+    this.webRequestService.get('lists').subscribe((lists) => {
+      this.getLists$.next(lists)
+    })
+    return this.getLists$
+  }
+
+  getTasks(listId: string) {
+    this.webRequestService.get(`lists/${listId}/tasks`).subscribe((tasks) => {
+      this.getTasks$.next(tasks)
+    })
+    return this.getTasks$
+  }
+
+  async createTask(title: string, listId: string) {
+    let result = await this.webRequestService.post(`lists/${listId}/tasks`, {title}).toPromise()
+    this.webRequestService.get(`lists/${listId}/tasks`).subscribe((tasks) => {
+      this.getTasks$.next(tasks)
+    })
+    return result
+  }
+
+  async patchTask(task: Task) {
+    return await this.webRequestService.patch(`lists/${task._listId}/tasks/${task._id}`, {task})
+  }
+}
